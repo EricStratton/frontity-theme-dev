53b0cca3ee93ad1a4e3b870c0230e1e5e0ec09b9
Eric Stratton <67491002+EricStratton@users.noreply.github.com>
2022-03-05 10:30:35 -0800

    implement changes

--- packages/theme-dev/src/components/index.js
+++ packages/theme-dev/src/components/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { connect, Global, css } from 'frontity';
+import gutembergStyle from './../styles/gutemberg/style.css';
+import gutembergTheme from './../styles/gutemberg/theme.css';
 
+import { fixCss } from './../helpers/css';
+import globalStyle from '../styles/theme/global-styles';
 import Layout from './layout';
-import themeStyles from './../styles/theme/themeStyles.css';
+import PostList from './post-list';
 
 const Root = ({ state }) => {
+  const colors = state.theme.colors;
+  const fixedGutembergStyle = fixCss(gutembergStyle);
+  const fixedGutembergTheme = fixCss(gutembergTheme);
+
   return (
     <>
-      <Global styles={css(themeStyles)} />
+      <Global styles={css(fixedGutembergStyle)} />
+      <Global styles={css(fixedGutembergTheme)} />
+      <Global styles={globalStyle(colors)} />
       <Layout>
-        <p>Current URL: {state.router.link}</p>
+        <main>
+          <PostList />
+        </main>
       </Layout>
     </>
   );
