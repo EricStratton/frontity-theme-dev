d361bece3508fd84f9d8da6a7522f84697fbf040
Eric Stratton <67491002+EricStratton@users.noreply.github.com>
2022-04-10 13:09:12 -0700

    render fetched footer data and basic styling

--- packages/theme-dev/src/components/layout/footer/footer-elements.js
+++ packages/theme-dev/src/components/layout/footer/footer-elements.js
@@ -1,6 +1,31 @@
 import { styled } from 'frontity';
 
 export const FooterContainer = styled.footer`
+  margin-top: auto;
   min-height: 10em;
   background-color: ${props => props.color};
+  display: flex;
+  justify-content: center;
+`;
+
+export const FooterWrapper = styled.div`
+  flex: 1;
+  max-width: clamp(20em, 80vw, 67.5em);
+  padding: 1em;
+
+  > .wp-block-group.is-footer .wp-block-columns {
+
+    > .wp-block-column {
+    
+      > .wp-block-group.footer__content-wrapper {
+        display: block;
+        margin: 0 auto;
+        width: fit-content;
+
+        & h2 {
+          margin-bottom: 0.5em;
+        }
+      }
+    }
+  }
 `;
--- packages/theme-dev/src/components/layout/footer/index.js
+++ packages/theme-dev/src/components/layout/footer/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { connect } from 'frontity';
 
 import {
   FooterContainer,
+  FooterWrapper,
 } from './footer-elements';
 
-const Footer = ({ colors }) => {
+const Footer = ({ state, libraries }) => {
+  const colors = state.theme.colors;
+  const data = state.source.get(`/footer/${state.theme.footerUrl}`);
+  const footer = state.source[data.type][data.id];
+  const Html2React = libraries.html2react.Component;
+
   return (
     <FooterContainer color={colors.tumble}>
-      This will be the footer.
+      <FooterWrapper>
+        <Html2React html={footer.content.rendered}/>
+      </FooterWrapper>
     </FooterContainer>
   )
 }
 
-export default Footer;
+export default connect(Footer);
